c307702d9b9bea7ff5e2e6566ed23ffc83f5a0ca
Luke Haase <luke.haase@gmx.ch>
2024-08-09 10:17:12 +0200

    Updated setup SQL Queries

--- src/assets/php/public/setup.php
+++ src/assets/php/public/setup.php
@@ -29,9 +29,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['password'])) {
         // create Scoreboard Admin Table
         $db->query("CREATE TABLE scoreboard_admins (ID int(11) NOT NULL AUTO_INCREMENT, username text NOT NULL, password text NOT NULL, PRIMARY KEY (ID)) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci");
         // create Eventperformance Table
-        $db->query("CREATE TABLE scoreboard_eventperformance ( ID int(11) NOT NULL AUTO_INCREMENT, eventid int(11) NOT NULL, personid int(11) NOT NULL, personname text NOT NULL, score int(11) NOT NULL, place text NOT NULL, PRIMARY KEY (ID)) ENGINE=InnoDB AUTO_INCREMENT=928 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci");
+        $db->query("CREATE TABLE scoreboard_eventperformance ( ID int(11) NOT NULL AUTO_INCREMENT, eventid int(11) NOT NULL, personid int(11) NOT NULL, score int(11) NOT NULL, place text NOT NULL, PRIMARY KEY (ID)) ENGINE=InnoDB AUTO_INCREMENT=928 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci");
         // create Events Table
-        $db->query("CREATE TABLE scoreboard_events ( ID int(11) NOT NULL AUTO_INCREMENT, name text NOT NULL, date date NOT NULL, type text NOT NULL, PRIMARY KEY (ID)) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci");
+        $db->query("CREATE TABLE scoreboard_events ( ID int(11) NOT NULL AUTO_INCREMENT, name text NOT NULL, date date NOT NULL, PRIMARY KEY (ID)) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci");
         // create Member Table
         $db->query("CREATE TABLE scoreboard_persons ( ID int(11) NOT NULL AUTO_INCREMENT, name text NOT NULL, PRIMARY KEY (ID)) ENGINE=InnoDB AUTO_INCREMENT=189 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci");
         
